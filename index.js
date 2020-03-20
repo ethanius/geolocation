9f24815144179a370cd0b221e86ab5c1dc860555
Kraft, Jakub <jakub.kraft@firma.seznam.cz>
2020-03-20 13:22:30 +0100

    prvni test deploy

---
+++ index.js
@@ -0,0 +1,35 @@
+var center = SMap.Coords.fromWGS84(14.41790, 50.12655);
+var m = new SMap(JAK.gel("m"), center, 13);
+m.addDefaultLayer(SMap.DEF_BASE).enable();
+m.addDefaultControls();
+
+var layer = new SMap.Layer.Marker();
+m.addLayer(layer);
+layer.enable();
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+/*
+var options = {};
+var marker = new SMap.Marker(center, "myMarker", options);
+layer.addMarker(marker);
+*/
